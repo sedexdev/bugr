42f00c7a3e2c5896f5e8881019622bf93ebec7dc
Andrew Macmillan <andrew.macmillan@ymail.com>
2020-06-05 21:07:35 +0100

    created deletion component for UI objects

---
+++ src/components/delete/Delete.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+
+import "./delete.css";
+
+export const Delete = ({ title, extraClasses, revealFunc }) => {
+    const deleteClasses = extraClasses
+        ? `delete-project-container ${extraClasses}`
+        : "delete-project-container";
+
+    return (
+        <div className={deleteClasses}>
+            <p className='delete-project-msg'>Delete {title}?</p>
+            <div className='delete-project-btn-container'>
+                <button className='delete-project-btn'>
+                    <i className='do-delete fas fa-check-circle'></i>
+                </button>
+                <button className='delete-project-btn'>
+                    <i
+                        className='do-not-delete fas fa-times-circle'
+                        onClick={() => revealFunc(false)}></i>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+Delete.propTypes = {
+    title: PropTypes.string,
+    extraClasses: PropTypes.string,
+    revealFunc: PropTypes.func,
+};
+
+export default Delete;
