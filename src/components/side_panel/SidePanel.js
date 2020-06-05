c1a65c9b06d33e3bdb66e520c3b84a32a9af43bd
Andrew Macmillan <andrew.macmillan@ymail.com>
2020-06-05 21:11:33 +0100

    add state and project list

--- src/components/side_panel/SidePanel.js
+++ src/components/side_panel/SidePanel.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
+import DeletePopUp from "../delete/Delete";
 
 import "./side_panel.css";
 
+const project = require("../../test_project_data/project_a.json");
+
 export const SidePanel = () => {
+    const [showDeleteProject, setDeleteProject] = useState(false);
+
     return (
         <aside className='side-panel-container'>
-            <h1>Side Panel</h1>
+            <div className='side-panel-heading-container'>
+                <h1 className='panel-heading'>Project Panel</h1>
+                <i
+                    className='add-project fas fa-plus-circle'
+                    title='New project'></i>
+            </div>
+            <div className='project-list-container'>
+                <div className='project-link'>
+                    <Router>
+                        <Link to='#'>{project.title}</Link>
+                    </Router>
+                    <i
+                        className='delete-project fas fa-times-circle'
+                        id={project.title}
+                        title='Delete project'
+                        onClick={() => setDeleteProject(true)}></i>
+                    {showDeleteProject && (
+                        <DeletePopUp
+                            title={project.title}
+                            revealFunc={setDeleteProject}
+                        />
+                    )}
+                </div>
+            </div>
         </aside>
     );
 };
