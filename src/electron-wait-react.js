c680d5b635459e20aa99461316b488537a5ff611
Andrew Macmillan <andrew.macmillan@ymail.com>
2020-07-29 14:42:25 +0100

    remove uneeded object key

--- src/electron-wait-react.js
+++ src/electron-wait-react.js
@@ -15,7 +15,7 @@ const client = new net.Socket();
 let startedElectron = false;
 const tryConnection = () =>
     // Open client connection to the port defined in process.env.PORT || port 3000
-    client.connect({ port: port }, () => {
+    client.connect({ port }, () => {
         client.end();
         if (!startedElectron) {
             console.log("starting electron");
