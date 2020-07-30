b3ab2ff25ce0eab8b8b83f5e6db164165a0bcffa
Andrew Macmillan <andrew.macmillan@ymail.com>
2020-07-30 15:23:00 +0100

    add logic to make panel responsive to user actions

--- src/components/side_panel/SidePanel.js
+++ src/components/side_panel/SidePanel.js
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 
 import DeletePopUp from "../delete/Delete";
 import AddPopUp from "../add/Add";
@@ -8,12 +7,26 @@ import PropTypes from "prop-types";
 
 import "./side_panel.css";
 
-const project = require("../../test_project_data/project_a.json");
-
-export const SidePanel = ({ createProject }) => {
+export const SidePanel = ({ loadProject, createProject, deleteProject }) => {
+    const [, updateState] = useState();
+    const [projectNamesIds, setProjectNamesIds] = useState([]);
     const [showDeleteProject, setDeleteProject] = useState(false);
+    const [projectLinkId, setProjectLinkId] = useState("");
     const [showAddProject, setAddProject] = useState(false);
     const [projectName, setProjectName] = useState("");
+
+    const forceUpdatePanel = useCallback(() => {
+        setTimeout(() => {
+            setProjectNamesIds(Object.keys(localStorage));
+            updateState({});
+        }, 1000);
+    }, []);
+
+    useEffect(() => {
+        if (Object.keys(localStorage)) {
+            setProjectNamesIds(Object.keys(localStorage));
+        }
+    }, []);
 
     const onChange = (e) => {
         setProjectName(e.target.value);
@@ -33,34 +46,63 @@ export const SidePanel = ({ createProject }) => {
                         name='projectName'
                         onChange={onChange}
                         revealFunc={setAddProject}
-                        onClick={() => createProject(projectName)}
+                        onClick={() => {
+                            createProject(projectName);
+                            forceUpdatePanel();
+                        }}
                     />
                 )}
             </div>
             <div className='project-list-container'>
-                <div className='project-link'>
-                    <Router>
-                        <Link to='#'>{project.title}</Link>
-                    </Router>
-                    <i
-                        className='delete-project fas fa-times-circle'
-                        id={project.title}
-                        title='Delete project'
-                        onClick={() => setDeleteProject(true)}></i>
-                    {showDeleteProject && (
-                        <DeletePopUp
-                            title={project.title}
-                            revealFunc={setDeleteProject}
-                        />
-                    )}
-                </div>
+                {projectNamesIds &&
+                    projectNamesIds.map((key) => {
+                        const value = localStorage.getItem(key);
+                        return key !== "currentData" ? (
+                            <div key={value} className='project-link'>
+                                <p className='project-link-title'>{key}</p>
+                                <i
+                                    className='delete-project fas fa-times-circle'
+                                    id={value}
+                                    title='Delete project'
+                                    onClick={() => {
+                                        setDeleteProject(true);
+                                        setProjectLinkId(value);
+                                    }}></i>
+                                {value === projectLinkId && showDeleteProject && (
+                                    <DeletePopUp
+                                        title={key}
+                                        revealFunc={setDeleteProject}
+                                        onClick={() => {
+                                            const currentData = JSON.parse(
+                                                localStorage.getItem(
+                                                    "currentData"
+                                                )
+                                            );
+                                            if (
+                                                currentData.project_name === key
+                                            ) {
+                                                localStorage.removeItem(
+                                                    "currentData"
+                                                );
+                                            }
+                                            localStorage.removeItem(key);
+                                            deleteProject(projectLinkId);
+                                            forceUpdatePanel();
+                                        }}
+                                    />
+                                )}
+                            </div>
+                        ) : null;
+                    })}
             </div>
         </aside>
     );
 };
 
 SidePanel.propTypes = {
+    loadProject: PropTypes.func.isRequired,
     createProject: PropTypes.func.isRequired,
+    deleteProject: PropTypes.func.isRequired,
 };
 
 export default SidePanel;
