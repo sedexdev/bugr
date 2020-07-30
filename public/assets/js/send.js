c9c736268359a329e3e41c4a7657db8f2f36b1eb
Andrew Macmillan <andrew.macmillan@ymail.com>
2020-07-30 15:19:27 +0100

    remove code duplication

--- public/assets/js/send.js
+++ public/assets/js/send.js
@@ -2,100 +2,162 @@ const path = require("path");
 const os = require("os");
 const { PythonShell } = require("python-shell");
 
-const fetchProjectNames = (callback) => {
-    const options = {
-        mode: "text",
-        pythonPath: `${
-            os.userInfo().homedir
-        }\\AppData\\Local\\Programs\\Python\\Python37\\python.exe`,
-        pythonOptions: ["-u"],
-        scriptPath: path.join(
-            __dirname,
-            "/",
-            "../",
-            "../",
-            "../",
-            "backend",
-            "projects"
-        ),
-        args: [],
-    };
+const options = {
+    mode: "text",
+    pythonPath: `${
+        os.userInfo().homedir
+    }\\AppData\\Local\\Programs\\Python\\Python37\\python.exe`,
+    pythonOptions: ["-u"],
+};
 
-    PythonShell.run("fetch_project_names.py", options, function (err, results) {
+const projectOptions = {
+    ...options,
+    scriptPath: path.join(
+        __dirname,
+        "/",
+        "../",
+        "../",
+        "../",
+        "backend",
+        "projects"
+    ),
+};
+
+const groupOptions = {
+    ...options,
+    scriptPath: path.join(
+        __dirname,
+        "/",
+        "../",
+        "../",
+        "../",
+        "backend",
+        "groups"
+    ),
+};
+
+const issueOptions = {
+    ...options,
+    scriptPath: path.join(
+        __dirname,
+        "/",
+        "../",
+        "../",
+        "../",
+        "backend",
+        "issues"
+    ),
+};
+
+/*
+===================================================
+              Project Level Functions
+===================================================
+*/
+
+const fetchProjectNames = (callback) => {
+    projectOptions.args = [];
+    PythonShell.run("fetch_project_names.py", projectOptions, function (
+        err,
+        results
+    ) {
         if (err) throw err;
         callback(results);
     });
 };
 
 const loadProject = (projectId, callback) => {
-    const options = {
-        mode: "text",
-        pythonPath: `${
-            os.userInfo().homedir
-        }\\AppData\\Local\\Programs\\Python\\Python37\\python.exe`,
-        pythonOptions: ["-u"],
-        scriptPath: path.join(
-            __dirname,
-            "/",
-            "../",
-            "../",
-            "../",
-            "backend",
-            "projects"
-        ),
-        args: [projectId],
-    };
-
-    PythonShell.run("load_project.py", options, function (err, results) {
+    projectOptions.args = [projectId];
+    PythonShell.run("load_project.py", projectOptions, function (err, results) {
         if (err) throw err;
         callback(results);
     });
 };
 
-const createProject = (projectName) => {
-    const options = {
-        mode: "text",
-        pythonPath: `${
-            os.userInfo().homedir
-        }\\AppData\\Local\\Programs\\Python\\Python37\\python.exe`,
-        pythonOptions: ["-u"],
-        scriptPath: path.join(
-            __dirname,
-            "/",
-            "../",
-            "../",
-            "../",
-            "backend",
-            "projects"
-        ),
-        args: [projectName],
-    };
+const createProject = (projectName, callback) => {
+    projectOptions.args = [projectName];
+    PythonShell.run("create_project.py", projectOptions, function (
+        err,
+        results
+    ) {
+        if (err) throw err;
+        callback(results);
+    });
+};
 
-    PythonShell.run("create_project.py", options, function (err) {
+const deleteProject = (projectId) => {
+    projectOptions.args = [projectId];
+    PythonShell.run("delete_project.py", projectOptions, function (err) {
         if (err) throw err;
     });
 };
 
-const createGroup = (projectName, projectId) => {
-    const options = {
-        mode: "text",
-        pythonPath: `${
-            os.userInfo().homedir
-        }\\AppData\\Local\\Programs\\Python\\Python37\\python.exe`,
-        pythonOptions: ["-u"],
-        scriptPath: path.join(
-            __dirname,
-            "/",
-            "../",
-            "../",
-            "../",
-            "backend",
-            "groups"
-        ),
-        args: [projectName, projectId],
-    };
+/*
+===================================================
+              Group Level Functions
+===================================================
+*/
 
-    PythonShell.run("create_group.py", options, function (err) {
+const createGroup = (projectName, projectId, groupName, callback) => {
+    groupOptions.args = [projectName, projectId, groupName];
+    PythonShell.run("create_group.py", groupOptions, function (err, results) {
+        if (err) throw err;
+        callback(results);
+    });
+};
+
+const deleteGroup = (projectId, groupId) => {
+    groupOptions.args = [projectId, groupId];
+    PythonShell.run("delete_group.py", groupOptions, function (err) {
+        if (err) throw err;
+    });
+};
+
+/*
+===================================================
+              Issue Level Functions
+===================================================
+*/
+
+const createIssue = (projectName, projectId, groupId) => {
+    issueOptions.args = [projectName, projectId, groupId];
+    PythonShell.run("create_issue.py", issueOptions, function (err) {
+        if (err) throw err;
+    });
+};
+
+const deleteIssue = (projectId, groupId, issueId) => {
+    issueOptions.args = [projectId, groupId, issueId];
+    PythonShell.run("delete_issue.py", issueOptions, function (err) {
+        if (err) throw err;
+    });
+};
+
+const editIssue = (projectId, groupId, issueId, description) => {
+    issueOptions.args = [projectId, groupId, issueId, description];
+    PythonShell.run("edit_issue.py", issueOptions, function (err) {
+        if (err) throw err;
+    });
+};
+
+const setDate = (projectId, groupId, issueId, date) => {
+    issueOptions.args = [projectId, groupId, issueId, date];
+    PythonShell.run("set_date.py", issueOptions, function (err) {
+        if (err) throw err;
+    });
+};
+
+const setPriority = (projectId, groupId, issueId, priority) => {
+    issueOptions.args = [projectId, groupId, issueId, priority];
+
+    PythonShell.run("set_priority.py", issueOptions, function (err) {
+        if (err) throw err;
+    });
+};
+
+const setStage = (projectId, groupId, issueId, stage) => {
+    issueOptions.args = [projectId, groupId, issueId, stage];
+    PythonShell.run("set_stage.py", issueOptions, function (err) {
         if (err) throw err;
     });
 };
@@ -104,5 +166,13 @@ module.exports = {
     fetchProjectNames,
     loadProject,
     createProject,
+    deleteProject,
     createGroup,
+    deleteGroup,
+    createIssue,
+    deleteIssue,
+    editIssue,
+    setDate,
+    setPriority,
+    setStage,
 };
