03c20800b2ccf9d0e75c4a795c29b1ade47c88a1
Andrew Macmillan <andrew.macmillan@ymail.com>
2020-07-30 15:22:14 +0100

    add welcome message

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 import IssueGroup from "../issue_group/IssueGroup";
 
@@ -6,10 +6,8 @@ import PropTypes from "prop-types";
 
 import "./main.css";
 
-const projectIssues = require("../../test_project_data/project_a.json");
-
 const Main = ({ createGroup }) => {
-    const [issueGroups, setIssueGroups] = useState([]);
+    const [currentData, setCurrentData] = useState(null);
     const [deleteGroupId, setDeleteGroupId] = useState("");
     const [addGroupId, setAddGroupId] = useState("");
     const [groupName, setGroupName] = useState("");
@@ -19,41 +17,70 @@ const Main = ({ createGroup }) => {
     const [dateOptionsId, setDateOptionsId] = useState("");
 
     useEffect(() => {
-        setIssueGroups(projectIssues["groups"]);
+        if (localStorage.getItem("currentData")) {
+            setCurrentData(JSON.parse(localStorage.getItem("currentData")));
+        }
     }, []);
 
     const onChangeGroupName = (e) => {
         setGroupName(e.target.value);
     };
 
+    const bugrIcon = require("../../images/bug.png");
+
     return (
         <main className='main-application-container'>
-            <h1 className='main-project-title'>{projectIssues.title}</h1>
-            {issueGroups.length &&
-                issueGroups.map((issueGroup) => {
-                    return (
-                        <IssueGroup
-                            key={issueGroup.id}
-                            issueGroupId={issueGroup.id}
-                            setAddGroupId={setAddGroupId}
-                            currentAddGroupId={addGroupId}
-                            setDeleteGroupId={setDeleteGroupId}
-                            currentDeleteGroupId={deleteGroupId}
-                            onChange={onChangeGroupName}
-                            title={issueGroup.title}
-                            issues={issueGroup.issues}
-                            prioritiesId={prioritiesId}
-                            setPrioritiesId={setPrioritiesId}
-                            stagesId={stagesId}
-                            setStagesId={setStagesId}
-                            issueOptionsId={issueOptionsId}
-                            setIssueOptionsId={setIssueOptionsId}
-                            dateOptionsId={dateOptionsId}
-                            setDateOptionsId={setDateOptionsId}
-                            onClick={() => createGroup(groupName)}
-                        />
-                    );
-                })}
+            {currentData ? (
+                <Fragment>
+                    <h1 className='main-project-title'>
+                        {currentData.project_name}
+                    </h1>
+                    {currentData.groups.map((issueGroup) => {
+                        return (
+                            <IssueGroup
+                                key={issueGroup.group_id}
+                                issueGroupId={issueGroup.group_id}
+                                setAddGroupId={setAddGroupId}
+                                currentAddGroupId={addGroupId}
+                                setDeleteGroupId={setDeleteGroupId}
+                                currentDeleteGroupId={deleteGroupId}
+                                onChange={onChangeGroupName}
+                                title={issueGroup.title}
+                                issues={issueGroup.issues}
+                                prioritiesId={prioritiesId}
+                                setPrioritiesId={setPrioritiesId}
+                                stagesId={stagesId}
+                                setStagesId={setStagesId}
+                                issueOptionsId={issueOptionsId}
+                                setIssueOptionsId={setIssueOptionsId}
+                                dateOptionsId={dateOptionsId}
+                                setDateOptionsId={setDateOptionsId}
+                                onClick={() =>
+                                    createGroup({
+                                        projectName: currentData.project_name,
+                                        projectId: currentData.project_id,
+                                        groupName,
+                                    })
+                                }
+                            />
+                        );
+                    })}
+                </Fragment>
+            ) : (
+                <div className='welcome-message'>
+                    <img
+                        className='bugr-icon-main'
+                        src={bugrIcon}
+                        alt='Loading'
+                    />
+                    <p>
+                        Welcome to Bugr! This app has been designed to make
+                        tracking issues in your application development cycle
+                        clean and simple.
+                    </p>
+                    <p>Use the Project Panel on the left to get started!</p>
+                </div>
+            )}
         </main>
     );
 };
