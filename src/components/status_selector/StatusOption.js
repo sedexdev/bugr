c3e1508a431ba2438769fe9d1b0ae0d3c85cd565
Andrew Macmillan <andrew.macmillan@ymail.com>
2020-07-31 15:33:23 +0100

    add logic to set priority and stage of issues

--- src/components/status_selector/StatusOption.js
+++ src/components/status_selector/StatusOption.js
@@ -2,12 +2,41 @@ import React from "react";
 
 import PropTypes from "prop-types";
 
-const StatusOption = ({ value }) => {
+const StatusOption = ({
+    value,
+    status,
+    setFunc,
+    setPrioritiesId,
+    setStagesId,
+    issueId,
+    issueGroupId,
+    updateAppState,
+}) => {
     const classes = `${value} status-color`;
-
     return (
         <div className='status-option'>
-            <div className={classes}>
+            <div
+                className={classes}
+                onClick={() => {
+                    const data = JSON.parse(
+                        localStorage.getItem("currentData")
+                    );
+                    const values = {
+                        projectId: data.project_id,
+                        groupId: issueGroupId,
+                        issueId,
+                    };
+                    if (status === "priority") {
+                        values.priority = value;
+                        setFunc(values);
+                        setPrioritiesId("");
+                    } else {
+                        values.stage = value;
+                        setFunc(values);
+                        setStagesId("");
+                    }
+                    updateAppState();
+                }}>
                 <p className='status-title'>{value}</p>
             </div>
         </div>
@@ -16,6 +45,13 @@ const StatusOption = ({ value }) => {
 
 StatusOption.propTypes = {
     value: PropTypes.string,
+    status: PropTypes.string,
+    setFunc: PropTypes.func,
+    setPrioritiesId: PropTypes.func,
+    setStagesId: PropTypes.func,
+    issueId: PropTypes.string,
+    issueGroupId: PropTypes.string,
+    updateAppState: PropTypes.func,
 };
 
 export default StatusOption;
